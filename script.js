ed61b4987c493dedfb7c9753c27a831329b40a25
sakilk130 <sakilk130@gmail.com>
2020-09-18 15:59:40 +0600

    finish

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const imgeContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImage = 0;
 let photosArray = [];
 
 //Fetch API
-const count = 10;
+const count = 5;
 const apiKey = 'UUqDH9rOXlYnClojNJntfvlz8ky5fno1KvVtZ5TmehY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 //**********************
+
+function imageLoaded() {
+  //console.log('image loaded');
+  imagesLoaded++;
+  //console.log(imagesLoaded);
+  if (imagesLoaded === totalImage) {
+    ready = true;
+    loader.hidden = true;
+    //console.log('Ready = ', ready);
+  }
+}
 
 function setAttributes(element, attributes) {
   for (const key in attributes) {
@@ -16,25 +30,26 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImage = photosArray.length;
+  // console.log('Total Images= ', totalImage);
+
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    // item.setAttribute('href', photo.links.html);
-    // item.setAttribute('target', '_blank');
     setAttributes(item, {
       href: photo.links.html,
       target: '_blank',
     });
 
     const img = document.createElement('img');
-    // img.setAttribute('src', photo.urls.regular);
-    // img.setAttribute('alt', photo.alt_description);
-    // img.setAttribute('title', photo.alt_description);
-
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+
+    img.addEventListener('load', imageLoaded);
+
     item.appendChild(img);
     imgeContainer.appendChild(item);
   });
@@ -54,11 +69,11 @@ async function getPhots() {
 //scrolling
 window.addEventListener('scroll', () => {
   if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
   ) {
+    ready = false;
     getPhots();
-    console.log('load more');
   }
 });
 
